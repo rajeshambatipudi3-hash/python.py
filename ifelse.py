ca39a046fb43575f0fb8b4452e04ba00310e88b7
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 17:22:39 +0530

    add

--- ifelse.py
+++ ifelse.py
@@ -22,3 +22,13 @@ elif age == 0: # this will check if the age is equal to 0
     print("You are not born yet.") # this will print if the age is equal to 0, which means the person is not born yet   
 else:
     print("You are a minor.") # this will print if the age is not greater than 18 and not equal to 0, which means the person is a minor 
+
+
+    age = int(input("Enter your age: ")) # this will take user input for age and convert it to an integer
+if age > 18: # this will check if the age is greater than 18
+    print("You are an adult.") # this will print if the age is greater than 18, which means the person is an adult      
+elif age == 0: # this will check if the age is equal to 0
+    print("You are not born yet.") # this will print if the age is equal to 0, which means the person is not born yet   
+else:
+    print("You are a minor.") # this will print if the age is not greater than 18 and not equal to 0, which means the person is a minor 
+    
