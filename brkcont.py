6c27ebbdd0c0149603d5fad37c16816e016e4f04
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 17:41:20 +0530

    add

---
+++ brkcont.py
@@ -0,0 +1,4 @@
+for i in range(0, 11): # this will loop from 0 to 10
+    if i == 5: # this will check if the current value of i is equal to 5
+        break # this will exit the loop if i is equal to 5
+    print(i) # this will print the current value of i in each iteration of the loop, which will be 0, 1, 2, 3, and 4
