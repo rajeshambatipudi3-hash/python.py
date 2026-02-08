d14382cd95900dfdef3f9e8a51189e9c7b2fe2ef
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 17:09:07 +0530

    new

---
+++ ifelse.py
@@ -0,0 +1,24 @@
+age = 0  # this will assign the value 10 to the variable age
+if age > 18: # this will check if the age is greater than 18
+    print("You are an adult.") # this will print if the age is greater than 18
+else: # this will execute if the age is not greater than 18
+    print("You are a minor.")   # this will print if the age is not greater than 18, which means the person is a minor
+age = 20 # this will assign the value 20 to the variable age
+if age > 18: # this will check if the age is greater than 18
+    print("You are an adult.") # this will print if the age is greater than 18, which means the person is an adult
+else:
+    print("You are a minor.")    # this will print if the age is not greater than 18, which means the person is a minor
+age = 0 # this will assign the value 0 to the variable age
+if age > 18: # this will check if the age is greater than 18
+    print("You are an adult.")  #   this will print if the age is greater than 18, which means the person is an adult
+elif age == 0: # this will check if the age is equal to 0
+    print("You are not born yet.") # this will print if the age is equal to 0, which means the person is not born yet
+else:
+    print("You are a minor.") # this will print if the age is not greater than 18 and not equal to 0, which means the person is a minor
+age = 15 # this will assign the value 15 to the variable age
+if age > 18: # this will check if the age is greater than 18
+    print("You are an adult.") # this will print if the age is greater than 18, which means the person is an adult  
+elif age == 0: # this will check if the age is equal to 0
+    print("You are not born yet.") # this will print if the age is equal to 0, which means the person is not born yet   
+else:
+    print("You are a minor.") # this will print if the age is not greater than 18 and not equal to 0, which means the person is a minor 
