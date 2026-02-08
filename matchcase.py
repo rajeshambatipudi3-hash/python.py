5f94a0f45a11ccfa018f50eb9828390ceaa83d63
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 17:28:58 +0530

    add

--- matchcase.py
+++ matchcase.py
@@ -11,3 +11,16 @@ match a:
         print("a is thirty") # this will print if a is equal to 30
     case _:
         print("a is something else") # this will print if a is not equal to 0 or 10, which means a is something else
+
+a = int(input("Enter a value for a: ")) # this will take user input for a and convert it to an integer
+match a:
+    case 0:
+        print("you won a car") # this will print if a is equal to 0
+    case 1:
+        print("you won a bike") # this will print if a is equal to 1
+    case 2:
+        print("you won a phone") # this will print if a is equal to 2
+    case 3:
+        print("you won a laptop") # this will print if a is equal to 3
+    case 4:
+        print("you won a tablet") # this will print if a is equal to 4
