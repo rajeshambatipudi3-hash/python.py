5043f3ae722487037d585f3f19167197a96d3a81
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 17:35:24 +0530

    add

--- loops.py
+++ loops.py
@@ -1,10 +1,13 @@
-print(1)
-for i in range(5): # this will loop from 0 to 4
-    print(i) # this will print the current value of i in each iteration of the loop
-print(2)
-for i in range(1, 6): # this will loop from 1 to 5
-    print(i) # this will print the current value of i in each iteration of the loop 
-print(3)
-for i in range(0, 10, 2): # this will loop from 0 to 9 with a step of 2
-    print(i) # this will print the current value of i in each iteration of the loop, which will be 0, 2, 4, 6, and 8    
-    
+# print(1)
+# for i in range(5): # this will loop from 0 to 4
+#     print(i) # this will print the current value of i in each iteration of the loop
+# print(2)
+# for i in range(1, 6): # this will loop from 1 to 5
+#     print(i) # this will print the current value of i in each iteration of the loop 
+# print(3)
+# for i in range(0, 10, 2): # this will loop from 0 to 9 with a step of 2
+#     print(i) # this will print the current value of i in each iteration of the loop, which will be 0, 2, 4, 6, and 8    
+
+
+for i in range(1, 11): # this will loop from 1 to 10
+    print(f"5 X {i} = {5 * i}") # this will print the multiplication table of 5, which will be 5 X 1 = 5, 5 X 2 = 10, and so on up to 5 X 10 = 50
