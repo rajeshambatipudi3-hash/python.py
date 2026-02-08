d84f1c4640e95922fd65403a4ee6b23cdf000a5f
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 15:20:15 +0530

    Add test.py with type conversion examples

---
+++ test.py
@@ -0,0 +1,7 @@
+a = 23 # this is an integer
+b = "43" # this is a string
+c = 123
+print(int(b)) # this will convert the string "43" to an integer 43
+print(str(a)) # this will convert the integer 23 to a string "23"
+c = str(b)
+print(type(c)) # this will print <class 'str'> since c is now a string  
