d7cfb01726ac5369b9570e36fd7491072c43ea2d
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 17:44:54 +0530

    add

--- brkcont.py
+++ brkcont.py
@@ -8,4 +8,10 @@ for i in range(0, 20): # this will loop from 0 to 19
         continue # this will skip the rest of the loop and move to the next iteration if i is equal to 10
     print(i) # this will print the current value of i in each iteration of the loop, which will be 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18 and 19 (10will be skipped)
 
+    print("Loop ended") # this will print "Loop ended" after the loop has finished executing    
+
+    print(i) # this will print the current value of i in each iteration of the loop, which will be 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18 and 19 (10 will be skipped)
+    if i == 10: # this will check if the current value of i is equal to 10
+        pass # this will do nothing and continue to the next iteration of the loop if i is equal to 10
     
+print("Loop ended") # this will print "Loop ended" after the loop has finished executing
