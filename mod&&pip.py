50066a1b64ff861c366f84548d15c3e5224e957b
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 18:49:58 +0530

    add

--- mod&&pip.py
+++ mod&&pip.py
@@ -15,3 +15,24 @@ print(math.ceil(2.3)) # this will print 3
 print(math.floor(2.7)) # this will print 2
 print(math.fabs(-5)) # this will print 5.0
 print(math.isqrt(16)) # this will print 4
+
+
+# pip install requests
+import requests
+response = requests.get('https://api.github.com')
+print(response.status_code)
+
+# pip install numpy
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+print(np.mean(arr))
+
+# pip install pandas
+import pandas as pd
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+print(df)
+
+# pip install matplotlib
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.savefig('plot.png')
