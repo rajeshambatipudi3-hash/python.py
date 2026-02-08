70e4eacae361b268e722bb1c469a5eb05ed90f7b
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 17:32:13 +0530

    add

---
+++ loops.py
@@ -0,0 +1,10 @@
+print(1)
+for i in range(5): # this will loop from 0 to 4
+    print(i) # this will print the current value of i in each iteration of the loop
+print(2)
+for i in range(1, 6): # this will loop from 1 to 5
+    print(i) # this will print the current value of i in each iteration of the loop 
+print(3)
+for i in range(0, 10, 2): # this will loop from 0 to 9 with a step of 2
+    print(i) # this will print the current value of i in each iteration of the loop, which will be 0, 2, 4, 6, and 8    
+    
