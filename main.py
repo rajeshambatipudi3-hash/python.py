ad24d462d8971a09b0e5617caea3682e91ddc5a1
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 14:02:51 +0530

    update

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# print("Hello, World")
-# print ("connected successfully to github  repository")
-# print("This is the first commit to github repository")
-# print("This is the second commit to github repository") 
-# print("This is the third commit to github repository")
-# print("This is the fourth commit to github repository") 
+print("Hello, World")
+print ("connected successfully to github  repository")
+print("This is the first commit to github repository")
+print("This is the second commit to github repository") 
+print("This is the third commit to github repository")
+print("This is the fourth commit to github repository") 
 
 
-# age = 25
-# cgpa = 3.5
-# name = "John Doe"
-# college = "ABC University"
-# mobile = "123-456-7890"
-# id_number = "123456789"
-# place = "New York"
-# city = "New York City"
-# state = "New York"
-# print(age, cgpa, name, college, mobile, id_number, place, city, state)
+age = 25
+cgpa = 3.5
+name = "John Doe"
+college = "ABC University"
+mobile = "123-456-7890"
+id_number = "123456789"
+place = "New York"
+city = "New York City"
+state = "New York"
+print(age, cgpa, name, college, mobile, id_number, place, city, state)
 
 age = 30
 cgpa = 3.8
