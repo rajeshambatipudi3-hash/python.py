ad13af972458ec5dc48de512bb8983645e2e2800
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 18:09:29 +0530

    add

---
+++ stringmethods.py
@@ -0,0 +1,6 @@
+name = "Alice"
+greeting = "Hello, " + name + "!"
+print(greeting) # this will print "Hello, Alice!"   
+name = "Alice"
+a =len(name) # this will calculate the length of the string "Alice", which is 5
+print(a) # this will print the value of a, which is 5
