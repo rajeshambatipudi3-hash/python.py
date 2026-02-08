8d4af1b10096de07ea4953cfc582be48b2ce154b
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 15:22:13 +0530

    Add user input functionality

--- test.py
+++ test.py
@@ -5,3 +5,6 @@ print(int(b)) # this will convert the string "43" to an integer 43
 print(str(a)) # this will convert the integer 23 to a string "23"
 c = str(b)
 print(type(c)) # this will print <class 'str'> since c is now a string  
+
+user_input = input("Enter a value: ")
+print(f"You entered: {user_input}")
