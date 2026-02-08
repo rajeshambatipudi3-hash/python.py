026d927374be4179da50be277b85e0ee9842bedc
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 15:09:03 +0530

    update

---
+++ test.py
@@ -0,0 +1,13 @@
+a = 32 # This is an integer
+b = "45" # This is a string
+d =223 # This is an integer
+print(a)
+print (b)
+print(type(a)) # This will print the type of variable a, which is <class 'int'>
+print(type(b)) # This will print the type of variable b, which is <class 'str'>
+c = int(b) # This will convert the string b to an integer and assign it to variable c
+print(c)
+print(type(c))  # This will print the type of variable c, which is <class 'int'>
+
+e = str(d) # This will convert the integer d to a string and assign it to variable e
+print(e)
