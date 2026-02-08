f8ffc0c7218b7e6f6ea345ef6b6d6911b4774957
rajesh <rajeshambatipudi01@gmail.com>
2026-02-08 14:24:25 +0530

    update

--- main.py
+++ main.py
@@ -30,3 +30,8 @@ float_number = 3.9
 print(int(age), str(name), float(float_number))
 
 # crreated new branch for backup and added new line to test the branch
+
+a = 10
+b = 20
+sum = a + b
+print("The sum of a and b is:", sum)
